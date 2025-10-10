87a1cd5589dd86bb4f00222604b5bf6e42a463f6
Preetam Kumar <103277784+Preetam9kumar@users.noreply.github.com>
2025-10-11 00:28:44 +0500

    Project frontend setup completed by installing react and tailwindcss.

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function  App() {
+  return (
+    <div className='bg-black text-amber-300 flex justify-center font-bold h-screen items-center text-4xl text-shadow-2xs text-shadow-blue-700'>
+      Welcome to StaySpot.pk
+    </div>
+  )
+}
+
+export default App;
